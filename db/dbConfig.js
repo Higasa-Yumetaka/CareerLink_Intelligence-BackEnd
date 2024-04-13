1519eb28bb4365f8505077d975605d687ba0e803
Higasa-Yumetaka <106534860+Higasa-Yumetaka@users.noreply.github.com>
2024-04-13 21:09:21 +0800

    Update dbConfig.js

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -3,7 +3,7 @@ const mysql = {
     port: '3305',
     user: 'root',
     password: '123456', // 密码
-    database: 'careerLink_intelligence', // 数据库
+    database: 'careerlink_intelligence', // 数据库
 
     connectTimeout: 5000 // 连接超时
 }
